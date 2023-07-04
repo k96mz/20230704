77fb88d8b62fdf8095369de1efa46df26b2b1af0
k96mz <65704916+k96mz@users.noreply.github.com>
2023-07-04 13:01:47 -0400

    Update main.js

--- docs/medical/main.js
+++ docs/medical/main.js
@@ -54,12 +54,45 @@ const map = new maplibregl.Map({
                 id: 'osm-layer',
                 source: 'osm',
                 type: 'raster',
+                layout: {visibility: 'none'}, // デフォルトでは非表示
             },
             {
                 id: 'gsi-layer',
                 source: 'gsi', // 使うデータをsourcesのkeyで指定する
                 type: 'raster', // データをどのように表示するか指定する
-                layout: {visibility: 'none'}, // デフォルトでは非表示
+                // layout: {visibility: 'none'}, // デフォルトでは非表示
+            },
+            {
+                id: 'skhb-layer-hospital',
+                source: 'skhb',
+                'source-layer': 'skhb',
+                type: 'circle',
+                paint: {
+                // 'circle-color': '#6666cc',
+                'circle-color': [
+                    // アイコンの色を属性値によって塗り分ける
+                    'interpolate',
+                    ['linear'],
+                    ['get', 'P04_001'], // P04_001は医療機関分類コードを示す
+                    1, 'rgba(255,0,0,1)', // 病院は赤　病院だけ表示とかしてみてもよい。
+                    //1, '#FF0000', // カラーコードは6桁でもできる。https://itsakura.com/html-color-codes
+                    //1, '#FF00001A', // 透過度は下2桁でできるらしいが、これはうまくいかない https://toriton.link/coding/opacity_colorcode/
+                    2, 'rgba(0,255,0,0)', // 診療所は緑
+                    // 3, '#00f', // 歯科診療所は青
+                    3, 'rgba(0,0,255,0)', // 歯科診療所は青
+                ],
+                'circle-radius': [ // ズームレベルに応じた円の大きさ
+                    'interpolate',
+                    ['linear'],
+                    ['zoom'],
+                    5,
+                    2,
+                    14,
+                    6,
+                ], 
+                // 'circle-stroke-width': 1,
+                // 'circle-stroke-color': '#ffffff',
+                },
             },
             {
                 id: 'skhb-layer-clinic',
@@ -67,7 +100,6 @@ const map = new maplibregl.Map({
                 'source-layer': 'skhb',
                 type: 'circle',
                 paint: {
-                // 'circle-color': '#6666cc',
                 'circle-color': [
                     // アイコンの色を属性値によって塗り分ける
                     'interpolate',
@@ -90,6 +122,35 @@ const map = new maplibregl.Map({
                 // 'circle-stroke-color': '#ffffff',
                 },
             },
+            {
+                id: 'skhb-layer-dentist',
+                source: 'skhb',
+                'source-layer': 'skhb',
+                type: 'circle',
+                paint: {
+                // 'circle-color': '#6666cc',
+                'circle-color': [
+                    // アイコンの色を属性値によって塗り分ける
+                    'interpolate',
+                    ['linear'],
+                    ['get', 'P04_001'], // P04_001は医療機関分類コードを示す
+                    1, 'rgba(255,0,0,0)', // 病院は赤
+                    2, 'rgba(0,255,0,0)', // 診療所は緑
+                    3, 'rgba(0,0,255,1)', // 歯科診療所は青
+                ],
+                'circle-radius': [ // ズームレベルに応じた円の大きさ
+                    'interpolate',
+                    ['linear'],
+                    ['zoom'],
+                    5,
+                    2,
+                    14,
+                    6,
+                ], 
+                // 'circle-stroke-width': 1,
+                // 'circle-stroke-color': '#ffffff',
+                },
+            },
         ],
     },
 });
@@ -98,9 +159,19 @@ map.on('load', () => {
     // 背景地図のコントロール
     const opacity = new OpacityControl({
         baseLayers: {
-            'osm-layer': 'OSM', // layer-id: レイヤー名
             'gsi-layer': '地理院地図',
+            'osm-layer': 'OSM', // layer-id: レイヤー名
         },
     });
     map.addControl(opacity, 'top-left'); // 第二引数で場所を指定
+
+    // 医療機関のコントロール
+    const opacitySkbh = new OpacityControl({
+        baseLayers: {
+            'skhb-layer-hospital': '病院：赤',
+            'skhb-layer-clinic': '診療所：緑',
+            'skhb-layer-dentist': '歯科医：青',
+        },    
+    });
+    map.addControl(opacitySkbh, 'top-right');
 });
